20c3f13ce56c26599a684d07f650bf0b38d5f3db
boumohamed <mb.bouzri@gmail.com>
2022-05-19 21:15:21 +0200

    here we go

---
+++ sendFile.py
@@ -0,0 +1,11 @@
+import pywhatkit
+
+
+
+# pywhatkit.sendwhatmsg("+tel", "Message 2", 13, 5, 2)
+# pywhatkit.playonyt("https://www.youtube.com/watch?v=ly8oNUH8Tyg&list=RDly8oNUH8Tyg&start_radio=1")
+# pywhatkit.start_server()
+# pywhatkit.text_to_handwriting("This Is My Firs HandWriting App", "test.png", (138, 0, 0))
+# pywhatkit.info("Python Language", 10)
+# pywhatkit.playonyt("Python")
+# pywhatkit.take_screenshot( "pywhatkit_screenshot", 2)
